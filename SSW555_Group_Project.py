b9a40ef443a1be22b9ab448bd8cffd41da195dc2
kevferreras <63133474+kevferreras@users.noreply.github.com>
2020-06-06 11:27:13 -0400

    Add files via upload

---
+++ SSW555_Group_Project.py
@@ -0,0 +1,87 @@
+'''The purpose of this program is to read a GEDCOM file, print "--> <input line>", and print "<-- <level>|<tag>|<valid?> : Y or N|<arguments>" '''
+
+from typing import Iterator, Tuple, IO, List, Dict
+
+class GedcomFile:
+    '''class GedcomFile'''
+
+    _valid_tags: Dict[str, str] = {  'INDI' : '0', 'NAME' : '1', 'SEX' : '1', 'BIRT' : '1', 'DEAT' : '1', 'FAMC' : '1',
+                                'FAMS' : '1', 'FAM' : '0', 'MARR' : '1', 'HUSB' : '1', 'WIFE' : '1', 'CHIL' : '1', 
+                                'DIV' : '1', 'DATE' : '2', 'HEAD' : '0', 'TRLR' : '0', 'NOTE' : '0', } #key = tag : value = level
+
+    def __init__(self) -> None:
+        '''Sets containers to store the input and output lines'''
+
+        self._input: List[str] = list()
+        self._output: List[str] = list()
+
+    def read_file(self, file_name: str) -> None:
+        '''Reads a GEDCOM file and populates the self._input list container with the lines from the GEDCOM file'''
+
+        file: IO = open(file_name)
+
+        with file:
+            for line in file:
+                self._input.append(line.strip())
+    
+    def validate_tags_for_output(self) -> None:
+        '''Takes each line in the self._input list container, splits it, and determines whether tags are valid'''
+
+        for line in self._input:
+            line: List[str] = line.split()
+            level: str = line[0]
+            tag: str = line[1]
+            arguments: str = f'{" ".join(line[2:])}'
+
+            if 'INDI' in line or 'FAM' in line:
+                self.validate_tags_for_exceptions(line, level, tag)
+            else:
+                if tag in GedcomFile._valid_tags and level == GedcomFile._valid_tags[tag]:
+                    self._output.append(f'<-- {level}|{tag}|Y|{arguments}')
+                else:
+                    self._output.append(f'<-- {level}|{tag}|N|{arguments}')
+      
+    def validate_tags_for_exceptions(self, line: List[str], level: str, default_tag: str) -> None:
+        '''Validates the tags for any line that meets the formatting exception identified in Project02 description.
+            Format exceptions identified are: '0 <id> INDI' and '0 <id> FAM'
+        '''
+
+        if line[-1] == 'INDI' or line[-1] == 'FAM':
+            tag: str = line[-1]
+            arguments: str = f'{line[1]}'
+
+            if tag in GedcomFile._valid_tags and level == GedcomFile._valid_tags[tag]:
+                self._output.append(f'<-- {level}|{tag}|Y|{arguments}')
+            else:
+                self._output.append(f'<-- {level}|{tag}|N|{arguments}')
+
+        else:
+            arguments: str = f'{line[-1]}'
+            self._output.append(f'<-- {level}|{default_tag}|N|{arguments}')
+
+    def write_to_txt_file(self, output_file_name: str) -> None:
+        '''Writes the final formatted input and output lines to a txt file'''
+
+        output_file: IO = open(f'{output_file_name}.txt', 'w')
+
+        with output_file:
+            for input_line, output_line in zip(self._input, self._output):
+                output_file.write(f'--> {input_line}\n{output_line}\n')
+        
+
+def main() -> None:
+    '''Creates an output file'''
+
+    file_name: str = input('Enter GEDCOM file name: ')
+    output_file_name: str = input('Enter desired name for the output file: ')
+
+    gedcom: GedcomFile = GedcomFile()
+    gedcom.read_file(file_name)
+    gedcom.validate_tags_for_output()
+    gedcom.write_to_txt_file(output_file_name)
+
+    print('Done. Your output file has been created.')
+
+
+if __name__ == '__main__':
+    main()
