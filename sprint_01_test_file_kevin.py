806c6ef878375dff8e8a2749a12134506ae9fbcb
kevferreras <kevferreras11@gmail.com>
2020-06-14 00:56:42 -0400

    Update to test file

--- sprint_01_test_file_kevin.py
+++ sprint_01_test_file_kevin.py
@@ -2,6 +2,7 @@
 
 from SSW555_Group_Project import Individual, GedcomFile
 import unittest
+from typing import IO
 
 class GedcomFileTest(unittest.TestCase):
     '''Tests the methods implemented for US30 & US31:
@@ -9,7 +10,11 @@ class GedcomFileTest(unittest.TestCase):
         -list_all_people_over_thirty_and_never_married
     '''
 
+    test_file_name: str = 'sprint_01_test_GEDCOM_file.txt'
+
     def test_list_all_people_living_and_married(self):
         '''Tests that the correct number and names of individuals are identified'''
 
+        gedcom: GedcomFile = GedcomFile()
+        gedcom.read_file(GedcomFileTest.test_file_name)
         
